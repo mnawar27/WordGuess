3b6f7dcef4d4d99dab05bef264a48490329fa063
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-10-29 19:30:37 -0400

    basic features work

--- src/main/java/com/github/zipcodewilmington/sample/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/sample/Wordguess.java
@@ -1,6 +1,32 @@
 package com.github.zipcodewilmington.sample;
+import java.util.Scanner;
 
 public class Wordguess {
 
     //Maisha Nawar
+    public String Lose() {
+
+        int tries = 0;
+        String userInput = "";
+        for (int i = 4; i > 0; i--) {
+            System.out.println("Let's Play Wordguess version 1.0");
+            System.out.println("Current Guesses:");
+            Scanner scanner = new Scanner(System.in);
+            userInput = scanner.nextLine();
+            //System.out.println(userInput);
+            tries = i;
+            System.out.println("You have " + tries + " tries");
+            System.out.println("Enter a single character:");
+            userInput = scanner.nextLine();
+        }
+
+        return userInput;
+
+    }
+
+    public static void main(String[] args){
+        Wordguess wg = new Wordguess();
+
+        wg.Lose();
+    }
 }
